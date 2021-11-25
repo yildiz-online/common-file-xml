8064ff81808472d045838d17655227dc14c3293f
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2021-11-25 18:49:06 +0100

    [FIX] Open is required on CI.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  SOFTWARE.
  */
 
-module be.yildizgames.common.file.xml {
+open module be.yildizgames.common.file.xml {
     requires java.desktop;
 
     requires be.yildizgames.common.file;
